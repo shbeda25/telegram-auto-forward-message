daba21ac40fa02188f9ea20ae5766bbb6fb928fd
shbeda25 <158088006+shbeda25@users.noreply.github.com>
2024-01-30 00:26:12 +0100

    Update forward.py

--- forward.py
+++ forward.py
@@ -1,4 +1,4 @@
 api_id= 26334525
 api_hash= 'ca40e2bffec6a0333dfb1b8f85c19225'
-client= Telegram Client ("+212669502771", api_id, api_hash)
+client= TelegramClient ("+212669502771", 26334525, 'ca40e2bffec6a0333dfb1b8f85c19225')
 client.start()
